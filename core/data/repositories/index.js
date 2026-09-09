dd62e3abfbdefc98cdcd03b187d27184c20343e5
wanstudio <99557053+wanstudio@users.noreply.github.com>
2026-09-10 04:55:02 +0700

    refactor(data): expose canonical repository boundary entrypoint

---
+++ core/data/repositories/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const CatalogRepository = require('./CatalogRepository');
+const OrderRepository = require('./OrderRepository');
+const PaymentRepository = require('./PaymentRepository');
+const PromotionRepository = require('./PromotionRepository');
+const DiningTableRepository = require('./DiningTableRepository');
+
+module.exports = {
+  CatalogRepository,
+  OrderRepository,
+  PaymentRepository,
+  PromotionRepository,
+  DiningTableRepository
+};
