309a49bb8f054fa57d18f7b86d60c3ea21a1bbdc
wanstudio <99557053+wanstudio@users.noreply.github.com>
2026-09-10 05:01:53 +0700

    refactor(data): export POS order repository from boundary index

--- core/data/repositories/index.js
+++ core/data/repositories/index.js
@@ -5,11 +5,13 @@ const OrderRepository = require('./OrderRepository');
 const PaymentRepository = require('./PaymentRepository');
 const PromotionRepository = require('./PromotionRepository');
 const DiningTableRepository = require('./DiningTableRepository');
+const PosOrderRepository = require('./PosOrderRepository');
 
 module.exports = {
   CatalogRepository,
   OrderRepository,
   PaymentRepository,
   PromotionRepository,
-  DiningTableRepository
+  DiningTableRepository,
+  PosOrderRepository
 };
